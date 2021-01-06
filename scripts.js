73edddd5789da636c5848485550beb43e38eedd6
xchanin <me@shannonbruns.com>
2021-01-05 22:54:49 -0700

    initial code push

---
+++ scripts.js
@@ -0,0 +1,42 @@
+// https://www.youtube.com/watch?v=RLc8NB2JyxE
+
+const sections = document.querySelectorAll('section');
+const bubble = document.querySelector('.bubble');
+const gradients = [
+    'linear-gradient(to right,#8ca6db,#b993d6)',
+    'linear-gradient(to right,#190a05,#870000)',
+    'linear-gradient(to right,#ff9472,#f2709c)'
+];
+
+const options = {
+    threshold: 0.7
+};
+
+let observer = new IntersectionObserver(navCheck, options);
+
+function navCheck(entries) {
+    entries.forEach(entry => {
+        const className = entry.target.className;
+        const activeAnchor = document.querySelector(`[data-page=${className}]`);
+        const gradientIndex = entry.target.getAttribute('data-index');
+        const coords = activeAnchor.getBoundingClientRect();
+        const directions = {
+            height: coords.height,
+            width: coords.width,
+            top: coords.top,
+            left: coords.left
+        }
+        if (entry.isIntersecting) {
+            bubble.style.setProperty('left', `${directions.left}px`);
+            bubble.style.setProperty('top', `${directions.top}px`);
+            bubble.style.setProperty('width', `${directions.width}px`);
+            bubble.style.setProperty('height', `${directions.height}px`);
+            bubble.style.background = gradients[gradientIndex];
+        }
+    });
+}
+
+sections.forEach(section => {
+    observer.observe(section);
+});
+
